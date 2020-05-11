109a17e71820dcb935ad0742818d7dcb46e6c3d4
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-11 11:58:33 -0300

    feat: add createPages to generate static pages

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.slug,
+        component: path.resolve('./src/templates/blog-post.js'),
+        context: {
+          slug: node.frontmatter.slug,
+        },
+      });
+    });
+  });
+};
