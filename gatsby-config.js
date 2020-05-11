25866f19f953490efd64ecb01aba86245b26682f
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-11 11:57:46 -0300

    refactor: change infos of manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,10 +34,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#38551f`,
+        theme_color: `#38551f`,
         display: `minimal-ui`,
-        icon: `src/images/logo.gif`, // This path is relative to the root of the site.
+        icon: `src/images/logos/logo-512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
