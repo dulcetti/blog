4d9d5b82562e5c25442cf847bb604da0855b1165
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-13 11:41:54 -0300

    Commons styles (#5)
    * style: theme with styles of blog
    - add colors object
    - add themes of fonts, grid and palette colors
    
    * style: add global reset css
    
    * style: add initial font of blog

---
+++ src/styles/fonts.js
@@ -0,0 +1,10 @@
+/* eslint-disable max-len */
+import { createGlobalStyle } from 'styled-components';
+
+export const Fonts = createGlobalStyle`
+  @font-face {
+    font-family: 'TitilliumWeb-Regular';
+    src: url('/assets/fonts/TitilliumWeb-Regular.ttf') format('truetype');
+    font-display: fallback;
+  }
+`;
