1ef800dd3095dc3886b1c0d3cf0eec6f4f25149e
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-13 11:36:41 -0300

    Blog header (#4)
    * feat: add logo on header
    
    * style: add style of actual blog on header
    
    * docs: remove content of readme
    
    * style: add logo image and remove page 2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,42 +1,35 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import * as S from './styles';
+import SearchBox from '../search-field';
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+export default function Header({ siteTitle }) {
+  const { logo } = useStaticQuery(
+    graphql`
+      query {
+        logo: file(relativePath: { eq: "logos/logo.png" }) {
+          childImageSharp {
+            fixed(height: 30, width: 95) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <S.Header>
+      <S.Container>
+        <S.LogoLink to="/">
+          <Img fixed={logo.childImageSharp.fixed} />
+          <S.Title>{siteTitle}</S.Title>
+        </S.LogoLink>
+
+        <SearchBox />
+      </S.Container>
+    </S.Header>
+  );
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
---
+++ src/components/header/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import { themes } from '../../styles/themes';
+
+export const Header = styled.header`
+  background-color: ${themes.palette.header.bg};
+  box-shadow: 0 1px 6px 0px #ccc;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: ${themes.grid.maxWidth};
+  padding: 10px 0;
+`;
+
+export const LogoLink = styled(Link)`
+  display: block;
+  position: relative;
+`;
+
+export const Title = styled.h1`
+  height: 0px;
+  overflow: hidden;
+  width: 0;
+`;
