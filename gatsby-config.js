522b877a6406e20aeae0af7dda2fd6a1ee65b620
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-13 22:27:26 -0300

    feat: add plugins of relative images and algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+const queries = require('./src/configs/algolia-queries');
+
 module.exports = {
   siteMetadata: {
     title: 'Bruno Dulcetti | web standards, css, xhtml e tecnologia em geral.',
-    description: 'Blog Pessoal de Bruno Dulcetti - Artigos, tutoriais sobre web standards, xhtml, css e tecnologia em geral.',
+    description:
+      'Blog Pessoal de Bruno Dulcetti - Artigos, tutoriais sobre web standards, xhtml, css e tecnologia em geral.',
     author: '@dulcetti',
+    siteUrl: 'https://www.brunodulcetti.com',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/images`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -23,21 +36,51 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWith: 980,
+              linkImagesToOriginal: false,
+            },
+          },
+          'gatsby-remark-lazy-load',
+          'gatsby-remark-prismjs',
+        ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bruno Dulcetti`,
+        short_name: `Bruno Dulcetti`,
         start_url: `/`,
         background_color: `#38551f`,
         theme_color: `#38551f`,
         display: `minimal-ui`,
-        icon: `src/images/logos/logo-512.png`, // This path is relative to the root of the site.
+        icon: `src/images/logos/logo-512.png`,
+        include_favicon: false,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
