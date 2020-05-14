d6c1ac54d2661bffe537bec88a036edc141dfd23
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-14 14:52:25 -0300

    feat: add iniitial search field component

---
+++ src/components/search-field/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  display: none;
+`;
+
+export const Icon = styled.div`
+  height: 32px;
+  width: 32px;
+
+  background: url('/images/icons/zoom.svg') no-repeat;
+`;
+
+export const InputSearch = styled.input`
+  background-color: #f7f8fa;
+  border-radius: 25px;
+  border: 1px solid #eeeff2;
+  color: #222;
+  font-size: 14px;
+  height: 28px;
+  min-width: 305px;
+  opacity: 1;
+`;
