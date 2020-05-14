6444fb3399a3632a884cd24561e2ee062d78fa70
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-13 22:39:05 -0300

    fix: add file config of queries on algolia (#9)

---
+++ src/configs/algolia-queries.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const postsQuery = `{
+  posts: allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+    edges {
+      node {
+        objectID: id
+        frontmatter {
+          category
+          date(locale: "pt-br", formatString: "DD[/]MM[/]YYYY")
+          date_timestamp: date
+          description
+          slug
+          title
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`;
+
+const flattenData = (dataArray) =>
+  dataArray.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt((new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)),
+    ...rest,
+  }));
+
+const settings = { attributesToSnippet: [`excerpt:20`] };
+
+const queries = [
+  {
+    query: postsQuery,
+    transformer: ({ data }) => flattenData(data.posts.edges),
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+    settings,
+  },
+];
+
+module.exports = queries;
