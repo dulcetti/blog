dc83cde22ac06c36e7a16622f2aa588bf5292c85
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-15 11:17:49 -0300

    feat: integration between sidebar and navigation - import icons component - add navigation component direct in sidebar - add method to toggle mobile sidebar - add social media list - add menu button

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 import * as S from './styles';
-import Navigation from '../navigation';
+import Icons from '../icons/index';
 
 export default function Sidebar({ siteTitle }) {
+  const [isOpen, setOpen] = useState(false);
   const location = typeof window !== 'undefined' && window.location.pathname;
   const { logo } = useStaticQuery(
     graphql`
@@ -21,6 +22,10 @@ export default function Sidebar({ siteTitle }) {
     `
   );
 
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <S.Sidebar>
       <S.LogoLink to="/">
@@ -32,9 +37,75 @@ export default function Sidebar({ siteTitle }) {
         )}
       </S.LogoLink>
 
-      <Navigation />
+      <S.Navigation className={isOpen && 'active'}>
+        <S.Menu>
+          <S.Name>Bruno Dulcetti</S.Name>
+          <S.List>
+            <S.Item>
+              <S.LinkMenu to="/">Home</S.LinkMenu>
+            </S.Item>
+            <S.Item>
+              <S.LinkMenu to="/sobre">Quem é?</S.LinkMenu>
+            </S.Item>
+            <S.Item>
+              <S.LinkMenu to="/curriculo">Currículo</S.LinkMenu>
+            </S.Item>
+            <S.Item>
+              <S.LinkMenu to="/contato">Contato</S.LinkMenu>
+            </S.Item>
+          </S.List>
+        </S.Menu>
 
-      <S.Text>© {new Date().getFullYear()}, Todos os fodasses reservados.</S.Text>
+        <S.SocialMedias>
+          <S.Media>
+            <a
+              href="https://github.com/dulcetti"
+              target="_blank"
+              rel="noopener noreferrer">
+              <Icons name="github" />
+            </a>
+          </S.Media>
+          <S.Media>
+            <a
+              href="https://www.linkedin.com/in/dulcetti/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <Icons name="linkedin" />
+            </a>
+          </S.Media>
+          <S.Media>
+            <a
+              href="https://twitter.com/dulcetti"
+              target="_blank"
+              rel="noopener noreferrer">
+              <Icons name="twitter" />
+            </a>
+          </S.Media>
+          <S.Media>
+            <a
+              href="https://instagram.com/dulcetti"
+              target="_blank"
+              rel="noopener noreferrer">
+              <Icons name="instagram" />
+            </a>
+          </S.Media>
+          <S.Media>
+            <a
+              href="https://facebook.com/dulcetti"
+              target="_blank"
+              rel="noopener noreferrer">
+              <Icons name="facebook" />
+            </a>
+          </S.Media>
+        </S.SocialMedias>
+      </S.Navigation>
+
+      <S.Text>
+        © {new Date().getFullYear()}, Todos os fodasses reservados.
+      </S.Text>
+      <S.ToggleOpenClose
+        onClick={toggleMenu}
+        className={isOpen && 'active'}></S.ToggleOpenClose>
     </S.Sidebar>
   );
 }
--- src/components/sidebar/styles.js
+++ src/components/sidebar/styles.js
@@ -5,12 +5,12 @@ import { themes } from '../../styles/themes';
 
 export const Sidebar = styled.aside`
   background-color: ${themes.palette.sidebar.bg};
-  box-shadow: 0 1px 6px 0 ${themes.palette.general.boxShadows};
-  display: grid;
-  grid-area: sidebar;
   position: fixed;
 
   ${media.greaterThan('medium')`
+    box-shadow: 0 1px 6px 0 ${themes.palette.general.boxShadows};
+    display: grid;
+    grid-area: sidebar;
     grid-template-rows: 50px auto 15px;
     min-height: 100vh;
     padding: 20px 0;
@@ -20,7 +20,12 @@ export const Sidebar = styled.aside`
   `}
 
   ${media.lessThan('767px')`
+    align-items: center;
     bottom: 0;
+    box-shadow: 0px 3px 6px 2px ${themes.palette.general.boxShadows};
+    display: flex;
+    justify-content: space-between;
+    padding: 0 15px;
     width: 100%;
   `}
 `;
@@ -29,6 +34,12 @@ export const LogoLink = styled(Link)`
   display: block;
   position: relative;
   text-align: center;
+
+  ${media.lessThan('medium')`
+    padding: 10px 0;
+    position: relative;
+    z-index: 2;
+  `}
 `;
 
 export const Title = styled.h1`
@@ -38,6 +49,7 @@ export const Title = styled.h1`
 `;
 
 export const InternalTitle = styled.strong`
+  display: block;
   height: 0;
   overflow: hidden;
   width: 0;
@@ -51,4 +63,116 @@ export const Text = styled.p`
   ${media.lessThan('large')`
     display: none;
   `}
+`;
+
+export const Navigation = styled.section`
+  ${media.lessThan('767px')`
+    background-color: ${themes.palette.sidebar.bgMobile};
+    border-bottom: 1px solid ${themes.palette.general.boxShadows};
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 55px);
+    justify-content: center;
+    left: 0;
+    position: fixed;
+    transform: translateX(-100%);
+    transition: all 0.5s ease 0s;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+
+    &.active {
+      transform: translateX(0);
+      transition: all 0.5s ease 0s;
+    }
+  `}
+
+  ${media.greaterThan('medium')`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `}
+`;
+
+export const Menu = styled.nav`
+  margin-bottom: 40px;
+`;
+
+export const List = styled.ul``;
+
+export const Item = styled.li`
+  border-bottom: 1px solid ${themes.palette.general.borders};
+  width: 100%;
+
+  &:first-child {
+    border-top: 1px solid ${themes.palette.general.borders};
+  }
+`;
+
+export const LinkMenu = styled(Link)`
+  color: ${themes.palette.sidebar.links};
+  display: block;
+  font-family: ${themes.fonts.others};
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 0;
+  text-align: center;
+  text-transform: uppercase;
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${themes.palette.sidebar.linksHover};
+    transition: color 0.3s;
+  }
+`;
+
+export const Name = styled.h2`
+  color: ${themes.palette.sidebar.name};
+  font-family: ${themes.fonts.titles};
+  font-size: 38px;
+  line-height: 40px;
+  margin-bottom: 30px;
+  text-align: center;
+`;
+
+export const ToggleOpenClose = styled.button`
+  ${media.greaterThan('769px')`
+    display:none;
+  `}
+
+  ${media.lessThan('medium')`
+    border: 0;
+    height: 32px;
+    width: 32px;
+
+    &:not(.active) {
+      background: url('/assets/images/icons/menu.svg') no-repeat;
+    }
+
+    &.active {
+      background: url('/assets/images/icons/close.svg') no-repeat;
+    }
+  `}
+`;
+
+export const SocialMedias = styled.ul`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Media = styled.li`
+  height: 32px;
+  margin: 0 5px;
+  width: 32px;
+
+  > a {
+    display: inline-block;
+    height: inherit;
+    width: inherit;
+
+    > svg {
+      height: inherit;
+      width: inherit;
+    }
+  }
 `;
