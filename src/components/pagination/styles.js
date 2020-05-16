2011b84323c9490f431837fe4152f40da49f3e2d
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-16 00:19:44 -0300

    feat(pagination): finish component - add svg icons - add single and arrows - put text of pages outside of wrap - add classNames to links - add className on wrap to verify if as a first or last page - add responsive styles of all component

--- src/components/pagination/styles.js
+++ src/components/pagination/styles.js
@@ -1,11 +1,106 @@
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import media from 'styled-media-query';
 
-export const PaginationWrap = styled.aside`
-  display: flex;
-  justify-content: space-between;
+import { themes } from '../../styles/themes';
+
+export const PaginationContent = styled.aside`
+  font-family: ${themes.fonts.titles};
+  text-align: center;
+
+  ${media.lessThan('large')`
+    padding: 0 10px;
+  `}
 `;
 
-export const ActionLinks = styled(Link)``;
+export const PaginationWrap = styled.nav`
+  align-items: center;
+  background-color: #ece7e1;
+  border-radius: 30px;
+  border: 3px solid #a68e69;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
 
-export const TextPages = styled.p``;
+  &.first-page {
+    justify-content: flex-end;
+  }
+
+  &.last-page {
+    justify-content: flex-start;
+  }
+`;
+
+export const ActionLinks = styled(Link)`
+  border-right: 1px solid #cec2ac;
+  border-left: 1px solid #f3f0ed;
+  color: #5a5038;
+  font-weight: bold;
+  font-size: 22px;
+  padding: 10px 0;
+  transition: background-color 0.3s, color 0.3s;
+  width: 25%;
+
+  &:hover {
+    background-color: #e3cfad;
+    color: #f26a00;
+    transition: background-color 0.3s, color 0.3s;
+
+    span {
+      margin: 0 20px;
+      transition: margin 0.3s;
+    }
+  }
+
+  &.first {
+    border-left: 0;
+    border-radius: 30px 0 0 30px;
+  }
+
+  &.first,
+  &.previous {
+    svg {
+      transform: rotate(180deg);
+    }
+  }
+
+  &.last {
+    border-right: 0;
+    border-radius: 0 30px 30px 0;
+  }
+`;
+
+export const TextPages = styled.p`
+  font-size: 18px;
+  line-height: 18px;
+`;
+
+export const Text = styled.span`
+  ${media.greaterThan('large')`
+    margin: 0 7px;
+    transition: margin .3s;
+  `}
+
+  ${media.lessThan('large')`
+    display: none;
+  `}
+`;
+
+export const Icon = styled.i`
+  display: inline-block;
+  height: 24px;
+  vertical-align: middle;
+  width: 24px;
+
+  > svg {
+    height: inherit;
+    width: inherit;
+  }
+`;
+
+export const ActualPage = styled.strong`
+  border-bottom: 2px solid #f26a00;
+  display: inline-block;
+  font-weight: bold;
+  line-height: 18px;
+`;
