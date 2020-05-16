b945682f31257de6901dd1cdbeafe047700b2b16
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-15 21:23:36 -0300

    refactor: rename component os search

--- src/components/search-field/index.js
+++ src/components/search-field/index.js
@@ -3,13 +3,17 @@ import * as S from './styles';
 
 import Icons from '../icons';
 
-export default function SearchBox() {
+export default function SearchField() {
   return (
     <S.Box>
       <S.Icon>
         <Icons name="search" />
       </S.Icon>
-      <S.InputSearch type="search" placeholder="Procure algum fodasse aqui" aria-label="Search through site content" />
+      <S.InputSearch
+        type="search"
+        placeholder="Procure algum fodasse aqui"
+        aria-label="Search through site content"
+      />
     </S.Box>
   );
 }
