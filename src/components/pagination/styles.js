ee7771e727cfdbe0d93176348b2dbf1658eaab2e
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-15 22:39:27 -0300

    style: add initial style of pagination

---
+++ src/components/pagination/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+export const PaginationWrap = styled.aside`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ActionLinks = styled(Link)``;
+
+export const TextPages = styled.p``;
