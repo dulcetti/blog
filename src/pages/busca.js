427a72bd65af10865703e3680be379f7a16ba765
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-16 22:44:52 -0300

    refactor: hide search component

--- src/pages/busca.js
+++ src/pages/busca.js
@@ -3,14 +3,12 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Search from '../components/search';
 
 export default function SearchPage() {
   return (
     <Layout>
       <SEO title="Busca" />
       <h1>Busca</h1>
-      <Search />
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   );
