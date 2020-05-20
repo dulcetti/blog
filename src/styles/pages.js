e78665d7ded95354072578f798c5b2270050a3a8
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-20 18:48:34 -0300

    feat: update contact page and add pages styles

---
+++ src/styles/pages.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+import { themes } from './themes';
+import SocialMedias from '../components/social-medias';
+
+export const PagesWrap = styled.div`
+  margin: auto;
+  max-width: 70rem;
+
+  ${media.lessThan('medium')`
+    padding: 5px 20px;
+  `}
+`;
+
+export const PageTitle = styled.h1`
+  font-size: min(60px, 10vw);
+  line-height: min(66px, 13vw);
+  margin-bottom: 1rem;
+`;
+
+export const PageText = styled.p`
+  font-size: 1.25rem;
+  margin-bottom: 2rem;
+`;
+
+export const SocialMediasContact = styled(SocialMedias)``;
