c10767485d6af13b14a4a5eea71c53a9277a8bb6
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-20 15:09:52 -0300

    feat: add transition bg color on link pages

--- src/pages/busca.js
+++ src/pages/busca.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
+import { themes } from '../styles/themes';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -9,7 +10,12 @@ export default function SearchPage() {
     <Layout>
       <SEO title="Busca" />
       <h1>Busca</h1>
-      <AniLink cover direction="left" bg="#F7F6F1" duration={0.6} to="/">
+      <AniLink
+        cover
+        direction="left"
+        bg={themes.palette.general.bgTransition}
+        duration={0.6}
+        to="/">
         Go back to the homepage
       </AniLink>
     </Layout>
