a9656211aab5b96a726e8bb474df6219608e4261
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-21 14:07:57 -0300

    refactor: change to pattern quotes on gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,20 @@
-const path = require('path');
+const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === 'MarkdownRemark') {
+
+  if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({
       node,
       getNode,
-      basePath: 'pages',
+      basePath: `pages`,
     });
 
     createNodeField({
+      name: `slug`,
       node,
-      name: 'slug',
       value: `/${slug.slice(12)}`,
     });
   }
