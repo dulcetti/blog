7ea563eb5581ba8bec76bfe4862b31c22eb04e4f
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-22 17:46:12 -0300

    feat: links list - add links const with label and slug - import links on sidebar - add map to list all links

---
+++ src/components/sidebar/links.js
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: 'Home',
+    slug: '/',
+  },
+  {
+    label: 'Sobre',
+    slug: '/sobre',
+  },
+  {
+    label: 'Busca',
+    slug: '/busca',
+  },
+  {
+    label: 'Contato',
+    slug: '/contato',
+  },
+];
