83ed5946bd2e41f7dabff2c2bf2ef3430f03ee0a
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-24 23:04:49 -0300

    feat: add all content and icons in about page

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -4,7 +4,8 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import { getAge } from '../utils';
+import Icons from '../components/icons';
+import { getAge, isMobile } from '../utils';
 
 import { themes } from '../styles/themes';
 import * as S from '../styles/pages/general';
@@ -14,14 +15,14 @@ export default function AboutPage() {
   const [scroll, setScroll] = useState(0);
   const { firstImage, secondImage, thirdImage } = useStaticQuery(graphql`
     query imagesAboutQuery {
-      firstImage: file(relativePath: { eq: "about/dulcetti-sorrindo.jpg" }) {
+      firstImage: file(relativePath: { eq: "about/dulcetti-hey-ho.jpg" }) {
         childImageSharp {
           fluid(maxHeight: 900, maxWidth: 600) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
-      secondImage: file(relativePath: { eq: "about/dulcetti-hey-ho.jpg" }) {
+      secondImage: file(relativePath: { eq: "about/dulcetti-sorrindo.jpg" }) {
         childImageSharp {
           fluid(maxHeight: 900, maxWidth: 600) {
             ...GatsbyImageSharpFluid_withWebp
@@ -42,81 +43,184 @@ export default function AboutPage() {
     setScroll(window.scrollY);
   };
 
-  typeof window !== 'undefined' && window.addEventListener('scroll', updateImage, false);
+  typeof window !== 'undefined' &&
+    !isMobile() &&
+    window.addEventListener('scroll', updateImage, false);
 
   return (
     <Layout>
-      <SEO title="Sobre" />
+      <SEO
+        description="Um cara curioso na área de tecnologia. Comecei na área de design gráfico e webdesign,
+              mas acabei migrando, felizmente, para o desenvolvimento."
+        image="/assets/images/about/dulcetti-rindo.jpg"
+        title="Sobre o Bruno Dulcetti"
+      />
       <SA.PageFluid>
         <SA.AboutSection>
           <SA.ColumnContent>
-            <S.PageTitle>Quenhé esse Bruno Dulcetti?</S.PageTitle>
-            <S.PageSubTitle>Nome Completo:</S.PageSubTitle>
-            <S.PageText>Bruno Rocha Dulcetti</S.PageText>
-            <S.PageSubTitle>Atuação:</S.PageSubTitle>
-            <S.PageText>Desenvolvimento / DevOps</S.PageText>
-            <S.PageSubTitle>Idade: {getAge()} anos</S.PageSubTitle>
-            <S.PageSubTitle>Localização:</S.PageSubTitle>
-            <S.PageText>Rio de Janeiro/RJ - Brasil</S.PageText>
+            <SA.TitleSection>"Quenhé" esse Bruno Dulcetti?</SA.TitleSection>
+            <S.PageText>
+              Um cara curioso na área de tecnologia. Comecei na área de design gráfico e webdesign,
+              mas acabei migrando, felizmente, para o desenvolvimento. Me especializei em Front-End,
+              o principal foco do meu{' '}
+              <AniLink
+                className="link-animated"
+                cover
+                direction="left"
+                bg={themes.palette.general.bgTransition}
+                duration={0.6}
+                to="/">
+                Blog
+              </AniLink>
+              .
+            </S.PageText>
+            <S.PageSubSubTitle>E qual a área de atuação desse cara?</S.PageSubSubTitle>
+            <S.PageText>
+              Atualmente atuo como Coordenador de Desenvolvimento na Wooza. Foi um belo desafio
+              mudar drasticamente, pois um coordenador não pega, quer dizer, não deveria pegar, em
+              código praticammente, mas sim cuidar dos times, Squads. Mas eu vou tentando, juro ;)
+            </S.PageText>
+            <S.PageText>
+              Estou com {getAge()} anos e uma coisa que eu gostava muito de fazer era dar aulas. Dei
+              aula na UniverCidade e na Faculdade CCAA, porém, ambas fecharam as portas. Estou sem
+              dar aulas desde o final de 2014, porém, pretendo voltar num futuro, quem sabe próximo.
+            </S.PageText>
+            <S.PageText>
+              Na Wooza eu tenho aprendido bastante, principalmente na área de DevOps e DevSecOps,
+              sendo esta segunda uma área que estou responsável. Falarei mais sobre ela no meu Blog,
+              tanto DevSecOps, quanto DevOps.
+            </S.PageText>
+            <S.PageSubSubTitle>E nas horas vagas, o que você faz?</S.PageSubSubTitle>
+            <S.PageText>
+              Sucesso! #brinks, adoro responder isso para os outros, a pergunta pede. Bom, muitos já
+              sabem que eu também sou músico e toco em vários blocos de rua no carnaval carioca e
+              também em São Paulo em algumas vezes. Vocês podem ver inclusive por causa da foto aqui
+              nessa parte.
+            </S.PageText>
+            <S.PageText>
+              Além disso, eu gosto muito de praia, tocar (na verdade estudar, pois não toco nada
+              ainda) meu trompete. E toco meu violão também, assim como o Ukulele, um excelente
+              instrumento para praticar. Fora ir pra praia, e claro, beber algumas cervejas.
+            </S.PageText>
+            <S.PageText>
+              Falando em cerveja, dá uma olhada no{' '}
+              <a
+                href="https://www.papodebar.com"
+                target="_blank"
+                className="link-animated"
+                rel="noopener noreferrer">
+                Papo de Bar
+              </a>
+              , meu site de bebidas alcoólicas.
+            </S.PageText>
           </SA.ColumnContent>
           <SA.ColumnImage className={scroll < 700 && 'active'}>
+            <SA.TitleSectionImage>"Quenhé"?</SA.TitleSectionImage>
             <SA.AboutImage fluid={firstImage.childImageSharp.fluid} />
           </SA.ColumnImage>
         </SA.AboutSection>
 
         <SA.AboutSection>
           <SA.ColumnContent>
-            <S.PageSubTitle>Formação:</S.PageSubTitle>
+            <SA.TitleSection>Um breve resumo</SA.TitleSection>
+            <S.PageSubSubTitle>Formação</S.PageSubSubTitle>
             <S.PageText>
-              Graduado em Gestão e Criação de Ambientes Internet na Unesa e Pós-Graduado em
-              Interface, Internet e Multimídia pela UFF
+              Bom, minha formação é num antigo curso chamado Gestão e Criação de Ambientes Internet,
+              pela Estácio de Sá. E fiz minha Pós-Graduação em Interface, Internet e Multimídia,
+              pela UFF. Tudo isso há um certo tempo, 2006 já estava pós-graduado.
             </S.PageText>
-            <S.PageSubTitle>Descrição:</S.PageSubTitle>
-            <S.PageSubSubTitle>Diz pra gente Bruno. Em que área você atua?</S.PageSubSubTitle>
+            <S.PageSubSubTitle>Por onde passou?</S.PageSubSubTitle>
             <S.PageText>
-              Nas peladas que rolam nos sábados eu atuo na Lateral Esquerda =D. Deixando a
-              brincadeira ridícula de fora, eu atuo na área de webdesign, design gráfico, há mais de
-              5 anos. Em todas as empresas em que trabalhei, fui o principal responsável pela
-              criação de todos os tipos de mídias digitais e impressas, tais como websites,
-              hotsites, folders, banners, outdoors, painéis, apresentações.
+              Qualquer coisa dá uma passada no{' '}
+              <a
+                href="https://www.linkedin.com/in/dulcetti/"
+                target="_blank"
+                className="link-animated"
+                rel="noopener noreferrer">
+                meu Linkedin
+              </a>
+              . Mas segue abaixo uma pequena lista mais atual.
             </S.PageText>
-            <S.PageSubSubTitle>Legal cara! E você estuda?</S.PageSubSubTitle>
-            <S.PageText>
-              Com certeza, todos os dias. Sou recém pós-graduado no curso de Interface, Internet e
-              Multimídia pela UFF em Niterói. Logo após a conclusão do curso, eu arrumei novamente
-              minhas malas (pra variar um pouco neh =P sou praticamente um nômade, um cigano =D)
-              mais uma vez e voltei pro Rio, minha terra natal e que amo bastante apesar da
-              violência, etc.
-            </S.PageText>
+            <SA.CareerList>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper>
+                    <Icons name="computer" />
+                  </SA.IconWrapper>
+                  Wooza
+                </SA.CareerCompany>
+                <SA.CareerTitle>Coordenador de desenvolvimento</SA.CareerTitle>
+                <SA.CareerDates>Jul 2019 – Present</SA.CareerDates>
+                <SA.CareerTitle>Arquiteto de soluções Front-End</SA.CareerTitle>
+                <SA.CareerDates>Jan 2018 – Jul 2019</SA.CareerDates>
+                <SA.CareerTitle>Desenvolvedor Front-End Sênior</SA.CareerTitle>
+                <SA.CareerDates>Jun 2016 – Jan 2018</SA.CareerDates>
+              </SA.CareerItem>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper>
+                    <Icons name="computer" />
+                  </SA.IconWrapper>
+                  Videolog
+                </SA.CareerCompany>
+                <SA.CareerTitle>CTO</SA.CareerTitle>
+                <SA.CareerDates>08/2013 – 01/15</SA.CareerDates>
+                <SA.CareerTitle>Gerente de Projetos</SA.CareerTitle>
+                <SA.CareerDates>04/2009 – 08/2013</SA.CareerDates>
+                <SA.CareerTitle>Desenvolvedor Front-End Sênior</SA.CareerTitle>
+                <SA.CareerDates>11/2005 – 04/2009</SA.CareerDates>
+              </SA.CareerItem>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper>
+                    <Icons name="computer" />
+                  </SA.IconWrapper>
+                  Mobicare
+                </SA.CareerCompany>
+                <SA.CareerTitle>Desenvolvedor Front-End Sênior</SA.CareerTitle>
+                <SA.CareerDates>11/2014 – 02/2016</SA.CareerDates>
+              </SA.CareerItem>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper className="graduation">
+                    <Icons name="graduation" />
+                  </SA.IconWrapper>
+                  Faculdade CCAA
+                </SA.CareerCompany>
+                <SA.CareerTitle>Professor das matérias WebDesign I e II</SA.CareerTitle>
+                <SA.CareerDates>08/2010 – 12/2014</SA.CareerDates>
+              </SA.CareerItem>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper>
+                    <Icons name="computer" />
+                  </SA.IconWrapper>
+                  Globo.com
+                </SA.CareerCompany>
+                <SA.CareerTitle>Desenvolvedor Front-End Sênior</SA.CareerTitle>
+                <SA.CareerDates>11/2006 – 04/2009</SA.CareerDates>
+              </SA.CareerItem>
+              <SA.CareerItem>
+                <SA.CareerCompany>
+                  <SA.IconWrapper className="graduation">
+                    <Icons name="graduation" />
+                  </SA.IconWrapper>
+                  UniverCidade
+                </SA.CareerCompany>
+                <SA.CareerTitle>Professor da matéria Interfaces Web</SA.CareerTitle>
+                <SA.CareerDates>08/2008 – 07/2010</SA.CareerDates>
+              </SA.CareerItem>
+            </SA.CareerList>
           </SA.ColumnContent>
           <SA.ColumnImage className={scroll >= 700 && scroll < 1500 && 'active'}>
+            <SA.TitleSectionImage>Resumo</SA.TitleSectionImage>
             <SA.AboutImage fluid={secondImage.childImageSharp.fluid} />
           </SA.ColumnImage>
         </SA.AboutSection>
 
         <SA.AboutSection>
           <SA.ColumnContent>
-            <S.PageSubSubTitle>
-              Sucesso total! além de estudar, você também curte essa área de estudos né?
-            </S.PageSubSubTitle>
-            <S.PageText>
-              Sim sim. Seja em fóruns, pelo meu blog, entre outros meios, gosto muito de ajudar os
-              outros. Fui instrutor na Formação Webdesigner em Petrópolis, no curso DigiSerra
-              Informática e este ano comecei a lecionar o módulo de Flash no curso iLearn. Gosto de
-              dar aulas, relação com pessoas e transmitir conhecimentos.
-            </S.PageText>
-            <S.PageSubSubTitle>Show de bola. Então me ajuda numa parada aqui?</S.PageSubSubTitle>
-            <S.PageText>
-              Sem exageros ok? Não faço consultoria gratuíta. Eu ajudo e tal, mas como diz um velho
-              dizer "Não entrego o peixe, eu ensino a pescar". Essa é minha teoria. Estude, corra
-              atrás, não queira tudo na sua mão.
-            </S.PageText>
-            <S.PageSubSubTitle>Legal Brunão. Vou continuar vendo seu Portfóliio.</S.PageSubSubTitle>
-            <S.PageText>
-              Seja bem vindo ae e volte sempre. Tentarei manter esse portfólio atualizado, inclusive
-              textos como esse. Qualquer coisa, entre em contato. ;]
-            </S.PageText>
-            <S.PageText>Aquele abraço.</S.PageText>
+            <SA.TitleSection>Um pouco das minhas Skills</SA.TitleSection>
             <AniLink
               className="link-animated"
               cover
@@ -128,6 +232,7 @@ export default function AboutPage() {
             </AniLink>
           </SA.ColumnContent>
           <SA.ColumnImage className={scroll >= 1500 && 'active'}>
+            <SA.TitleSectionImage>Skills</SA.TitleSectionImage>
             <SA.AboutImage fluid={thirdImage.childImageSharp.fluid} />
           </SA.ColumnImage>
         </SA.AboutSection>
