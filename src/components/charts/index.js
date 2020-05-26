276c12fdb076cae33b7117460909bd5ddbf1a65d
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-26 13:25:40 -0300

    feat: add three charts for responsive devices

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import { HorizontalBar, Bar } from 'react-chartjs-2';
 
-import { mySkills, optionsBar } from './skills';
+import * as S from './styles';
+import { myAllSkills, myFirstSkills, mySecondSkills, optionsBar } from './skills';
 
 export default function MySkillCharts() {
-  return <HorizontalBar data={mySkills} options={optionsBar} />;
+  return (
+    <S.SkillCharts>
+      <S.ChartFullColumn>
+        <HorizontalBar data={myAllSkills} options={optionsBar} />
+      </S.ChartFullColumn>
+      <S.ChartColumn>
+        <Bar data={myFirstSkills} options={optionsBar} />
+      </S.ChartColumn>
+      <S.ChartColumn>
+        <Bar data={mySecondSkills} options={optionsBar} />
+      </S.ChartColumn>
+    </S.SkillCharts>
+  );
 }
