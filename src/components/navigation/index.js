6deea9879a91f6fc97cf8a0b1b9ca934f5b3084a
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-05-30 21:39:49 -0300

    feat: add links on thumb and name of sidebar

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
 import * as S from './styles';
 import { themes } from '../../styles/themes';
@@ -24,8 +25,23 @@ export default function Navigation({ isOpen }) {
   return (
     <S.Navigation className={isOpen && 'active'}>
       <S.Menu>
-        <S.Thumb fluid={avatar.childImageSharp.fluid} />
-        <S.Name>Bruno Dulcetti</S.Name>
+        <AniLink
+          swipe
+          direction="left"
+          bg={themes.palette.general.bgTransition}
+          duration={0.6}
+          to="/">
+          <S.Thumb fluid={avatar.childImageSharp.fluid} />
+        </AniLink>
+
+        <AniLink
+          swipe
+          direction="left"
+          bg={themes.palette.general.bgTransition}
+          duration={0.6}
+          to="/">
+          <S.Name>Bruno Dulcetti</S.Name>
+        </AniLink>
         <S.List>
           {links.map((link, index) => (
             <S.Item key={index}>
