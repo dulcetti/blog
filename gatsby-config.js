b08c94f1c3b8cc97d3b5da86eb85bcd5c6ac4fb6
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-03 19:40:12 -0300

    Build algolia (#63)
    * refactor: update gatsby config
    - create metadata const
    - create plugins const
    - move the algolia and anaytics configs to anothers consts
    - add algolia and analytics inside production conditional
    - push algolia and analytics on plugins config array
    - export the module
    
    * chore: update version to 1.2.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,105 +1,86 @@
 require('dotenv').config();
 const queries = require('./src/utils/algolia-queries');
 
-module.exports = {
-  siteMetadata: {
-    title: 'Bruno Dulcetti | web standards, css, xhtml e tecnologia em geral.',
-    description:
-      'Blog Pessoal de Bruno Dulcetti - Artigos, tutoriais sobre web standards, xhtml, css e tecnologia em geral.',
-    author: '@dulcetti',
-    siteUrl: 'https://www.brunodulcetti.com',
+const metadata = {
+  author: '@dulcetti',
+  description:
+    'Blog Pessoal de Bruno Dulcetti - Artigos, tutoriais e dicas sobre DevOps, Front-End, CSS, Agile, Git e Tecnologia em geral.',
+  siteUrl: 'https://www.brunodulcetti.com',
+  title: 'Bruno Dulcetti | DevOps, Front-End, CSS, Agile, Git e Tecnologia em geral.',
+};
+
+const pluginConfig = [
+  `gatsby-plugin-react-helmet`,
+  // needs to be the first to work with gatsby-remark-images
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `uploads`,
+      path: `${__dirname}/static/uploads`,
+    },
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    // needs to be the first to work with gatsby-remark-images
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/uploads`,
-      },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/static/assets/images`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/static/assets/images`,
-      },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/posts`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-relative-images',
+          options: {
+            name: 'uploads',
           },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWith: 980,
-              linkImagesToOriginal: false,
-            },
+        },
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWith: 980,
+            linkImagesToOriginal: false,
           },
-          'gatsby-remark-lazy-load',
-          'gatsby-remark-prismjs',
-          'gatsby-remark-external-links',
-          'gatsby-remark-responsive-iframe',
-        ],
-      },
+        },
+        'gatsby-remark-lazy-load',
+        'gatsby-remark-prismjs',
+        'gatsby-remark-external-links',
+        'gatsby-remark-responsive-iframe',
+      ],
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-transition-link`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries,
-        chunkSize: 10000,
-        enablePartialUpdates: true,
-      },
+  },
+  `gatsby-plugin-styled-components`,
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  `gatsby-plugin-transition-link`,
+  `gatsby-plugin-sitemap`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Bruno Dulcetti`,
+      short_name: `Bruno Dulcetti`,
+      start_url: `/`,
+      background_color: `#046A2E`,
+      theme_color: `#046A2E`,
+      display: `minimal-ui`,
+      icon: `./static/assets/images/logos/logo-512.png`,
+      include_favicon: false,
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Bruno Dulcetti`,
-        short_name: `Bruno Dulcetti`,
-        start_url: `/`,
-        background_color: `#046A2E`,
-        theme_color: `#046A2E`,
-        display: `minimal-ui`,
-        icon: `./static/assets/images/logos/logo-512.png`,
-        include_favicon: false,
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-        head: false,
-        defer: true,
-      },
-    },
-    `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+  },
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-netlify-cms`,
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
           {
             site {
               siteMetadata {
@@ -111,20 +92,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                });
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.edges.map((edge) => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ 'content:encoded': edge.node.html }],
               });
-            },
-            query: `
+            });
+          },
+          query: `
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
@@ -143,12 +124,42 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
-            title: 'Bruno Dulcetti - Blog',
-            link: 'https://feeds.feedburner.com/brunodulcetti',
-          },
-        ],
-      },
+          output: '/rss.xml',
+          title: 'Bruno Dulcetti - Blog',
+          link: 'https://feeds.feedburner.com/brunodulcetti',
+        },
+      ],
     },
-  ],
+  },
+];
+
+if (process.env.CONTEXT === 'production') {
+  const algolia = {
+    resolve: `gatsby-plugin-algolia-search`,
+    options: {
+      appId: process.env.GATSBY_ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+      queries,
+      chunkSize: 10000,
+      enablePartialUpdates: true,
+    },
+  };
+
+  const analytics = {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      head: false,
+      defer: true,
+    },
+  };
+
+  pluginConfig.push(algolia);
+  pluginConfig.push(analytics);
+}
+
+module.exports = {
+  siteMetadata: metadata,
+  plugins: pluginConfig,
 };
