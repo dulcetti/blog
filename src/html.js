2b9be6a39540003ad0b8ddd348a703f2ae71ca50
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-11 00:06:45 -0300

    fix: add cloudfront url on csp metatag

--- src/html.js
+++ src/html.js
@@ -14,13 +14,13 @@ export default function HTML(props) {
           content="
             base-uri 'self';
             connect-src 'self' data: gap: ws:* ssl.gstatic.com 'unsafe-inline' links.services.disqus.com;
-            font-src 'self' data:;
+            font-src 'self' data: *.cloudfront.net;
             frame-src 'self' https://disqus.com/;
-            img-src 'self' data: *.google-analytics.com *.viglink.com links.services.disqus.com ce.lijit.com;
+            img-src 'self' data: *.google-analytics.com *.viglink.com links.services.disqus.com ce.lijit.com *.cloudfront.net;
             manifest-src 'self';
             media-src 'self';
             object-src 'none';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google-analytics.com *.disqus.com *.disquscdn.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google-analytics.com *.disqus.com *.disquscdn.com *.cloudfront.net;
             style-src 'self' 'unsafe-inline' blob: *.disquscdn.com"
         />
         {props.headComponents}
