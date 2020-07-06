7d622001e8a40d7b40bee3677d55dee8e96be5f2
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-07-06 20:50:53 -0300

    style: add vars to styles

---
+++ src/components/breadcrumb/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
+export const BreadcrumbWrapper = styled.p``;
+
+export const HomeLink = styled(AniLink)``;
+
+export const CategoryLink = styled.span``;
+
+export const Title = styled.strong``;
