0bd6e6aea952da88c3aa73f0955b7b6bb3cd7149
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-07-06 21:36:25 -0300

    refactor: add classname and symbols on html

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import * as S from './styles';
-import { PropTypes } from 'prop-types';
 
 export default function Breadcrumb({ category, title }) {
   return (
     <S.BreadcrumbWrapper>
-      <S.HomeLink to="/">Home</S.HomeLink> &raquo;
-      <S.CategoryLink>{category}</S.CategoryLink> &rsaquo;
-      <S.Title>{title}</S.Title>
+      <S.HomeLink className="link-animated" to="/">
+        @dulcetti
+      </S.HomeLink>{' '}
+      / <S.CategoryLink>{category}</S.CategoryLink> / <S.Title>{title}</S.Title>
     </S.BreadcrumbWrapper>
   );
 }
