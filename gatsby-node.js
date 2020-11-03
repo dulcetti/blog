73754e8b966fdbbbe171d3694a933e8576fe5f19
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-11-03 11:41:29 -0300

    refactor: remove fmImagesToRelative function

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,9 @@
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
-const { fmImagesToRelative } = require(`gatsby-remark-relative-images`);
 
 // To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  fmImagesToRelative(node);
 
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({
